d3b56c8fe2a83ea7a138e88cc6aa72286e309444
vercettiprada <xaviersaligumba@gmail.com>
2025-04-07 23:03:52 +0800

    register routers

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,16 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+//🔒🔒🔑
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('register', 'AuthController@register');
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('user-profile', 'AuthController@me');
+});
+
 $router->get('/version', function () use ($router) {
     return $router->app->version();
 });
