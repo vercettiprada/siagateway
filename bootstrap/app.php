ee854192b2f961c1f09e62dd0f63b4da95ec0dbb
vercettiprada <xaviersaligumba@gmail.com>
2025-04-07 23:48:51 +0800

    enabled middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -67,13 +67,13 @@ $app->singleton(
 | route or middleware that'll be assigned to some specific routes.
 |
 */
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+ $app->middleware([
+     App\Http\Middleware\ExampleMiddleware::class
+ ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+ $app->routeMiddleware([
+     'auth' => App\Http\Middleware\Authenticate::class,
+ ]);
 
 /*
 |--------------------------------------------------------------------------
@@ -85,7 +85,7 @@ $app->singleton(
 | totally optional, so you are not required to uncomment this line.
 |
 */
-// $app->register(App\Providers\AppServiceProvider::class);
+ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
