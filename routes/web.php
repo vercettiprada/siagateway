eed776b21aa753a45806b83c105303637df5a8a9
vercettiprada <xaviersaligumba@gmail.com>
2025-04-07 23:04:23 +0800

    grouped router middleware

--- routes/web.php
+++ routes/web.php
@@ -31,20 +31,24 @@ $router->get('/version', function () use ($router) {
     return $router->app->version();
 });
 
-// API GATEWAY ROUTES FOR SITE1 users
-$router->get('/users1', 'User1Controller@index');
-$router->post('/users1', 'User1Controller@add');            // create new user record
-$router->get('/users1/{id}', 'User1Controller@show');       // get user by id
-$router->put('/users1/{id}', 'User1Controller@update');     // update user record
-$router->patch('/users1/{id}', 'User1Controller@update');   // update user record
-$router->delete('/users1/{id}', 'User1Controller@delete');  // delete record
+$router->group(['middleware' => 'auth:api'], function () use ($router) {
 
-// API GATEWAY ROUTES FOR SITE2 users
-$router->get('/users2', 'User2Controller@index');
-$router->post('/users2', 'User2Controller@add');            // create new user record
-$router->get('/users2/{id}', 'User2Controller@show');       // get user by id
-$router->put('/users2/{id}', 'User2Controller@update');     // update user record
-$router->patch('/users2/{id}', 'User2Controller@update');   // update user record
-$router->delete('/users2/{id}', 'User2Controller@delete');  // delete record
 
+    $router->get('/users1', 'User1Controller@index');
+    $router->post('/users1', 'User1Controller@add');           
+    $router->get('/users1/{id}', 'User1Controller@show');       
+    $router->put('/users1/{id}', 'User1Controller@update');    
+    $router->patch('/users1/{id}', 'User1Controller@update');   
+    $router->delete('/users1/{id}', 'User1Controller@delete');  
+});
+
+
+$router->group(['middleware' => 'auth:api'], function () use ($router) {
+    $router->get('/users2', 'User2Controller@index');
+    $router->post('/users2', 'User2Controller@add');            
+    $router->get('/users2/{id}', 'User2Controller@show');       
+    $router->put('/users2/{id}', 'User2Controller@update');    
+    $router->patch('/users2/{id}', 'User2Controller@update');   
+    $router->delete('/users2/{id}', 'User2Controller@delete');  
+});
 
