0439f2a249abe4c2990b8d5044557a1f447d5d6a
vercettiprada <xaviersaligumba@gmail.com>
2025-04-07 22:45:42 +0800

    readded jwt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -86,7 +86,9 @@ $app->singleton(
 |
 */
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
