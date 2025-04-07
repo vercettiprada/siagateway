6932b551d7ce2563a4fdaceeb079f743cac7e477
vercettiprada <xaviersaligumba@gmail.com>
2025-04-07 23:10:15 +0800

    modified auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Laravel\Lumen\Auth\Authorizable;
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
-
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
